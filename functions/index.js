217377878efc0b173024b02cdb49cb7d15ed3f78
Jay <walkccray@gmail.com>
2018-12-19 00:48:57 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -2,42 +2,60 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello, Taiwan!");
-});
-
 const createNotification = (notification => {
   return admin.firestore().collection('notifications')
     .add(notification)
     .then(doc => console.log('notification added', doc))
 })
 
-exports.postCreated = functions.firestore
-  .document('posts/{postId}')
-  .onCreate(doc => {
-
-    const post = doc.data()
+exports.addPost = functions.firestore
+  .document('posts/{pid}')
+  .onCreate(snap => {
+    const post = snap.data()
     const notification = {
-      content: 'Added a new post',
+      content: `Added '${post.title}'`,
       user: `${post.authorFirstName} ${post.authorLastName}`,
       time: admin.firestore.FieldValue.serverTimestamp()
     }
+    return createNotification(notification)
+})
 
+exports.updatePost = functions.firestore
+  .document('posts/{pid}')
+  .onUpdate(snap => {
+    const post = snap.data()
+    const notification = {
+      content: `Updated '${post.title}'`,
+      user: `${post.authorFirstName} ${post.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+    return createNotification(notification)
+})
+
+exports.deletePost = functions.firestore
+  .document('posts/{pid}')
+  .onDelete(snap => {
+    const post = snap.data()
+    const notification = {
+      content: `Deleted '${post.title}'`,
+      user: `${post.authorFirstName} ${post.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
     return createNotification(notification)
 })
 
 exports.userJoined = functions.auth.user()
   .onCreate(user => {
-
     return admin.firestore().collection('users')
-      .doc(user.uid).get().then(doc => {
-        const newUser = doc.data()
+      .doc(user.uid)
+      .get()
+      .then(snap => {
+        const newUser = snap.data()
         const notification = {
           content: 'Joined the party',
           user: `${newUser.firstName} ${newUser.lastName}`,
           time: admin.firestore.FieldValue.serverTimestamp()
         }
-
         return createNotification(notification)
       })
 })
